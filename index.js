67cce2a9ccb2a50234d5d621b92a0c78e9397751
shivansh113 <shivanshd@gmail.com>
2024-02-22 16:56:39 -0700

    Adding new card button as well as ace card functionality

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-let firstCard = 12
-let secondCard = 12
-
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false;
 let isInGame = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEL =document.querySelector("#cards-el")
 
+function getRandomCard() {
+    randNum = parseInt((Math.random() * 9) + 1)
+    let checkForAce = 0
+    if (randNum == 1) {
+        if ((checkForAce += sum + randNum) > 21) {
+            return randNum
+        } else {
+            return 11;
+        }
+    }
+    return randNum
+}
 
 function startGame() {
+    renderGame();
+}
 
+function renderGame() {
+    cardsEL.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEL.textContent += cards[i] + " ";
+    }
+    sumEl.textContent = "Sum: " + sum
     if (sum < 21) {
         message = "Hit or hold"
     } else if (sum == 21) {
@@ -18,5 +41,17 @@ function startGame() {
         message = "Bust"
         isInGame = false;
     }
+    messageEl.textContent = message;
+}
+
+function newCardpull() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+
+function aceCardFound() {
 
 }
+
