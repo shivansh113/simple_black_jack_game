4f7732e21678d92c62ea14d862ad098ce573b58e
shivansh113 <shivanshd@gmail.com>
2024-02-22 15:51:14 -0700

    Adding file and few bits of code that change UI

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 12
+let secondCard = 12
 
 let sum = firstCard + secondCard
+let hasBlackJack = false;
+let isInGame = true
+let message = ""
 
+
+function startGame() {
+
+    if (sum < 21) {
+        message = "Hit or hold"
+    } else if (sum == 21) {
+        message = "Blackjack"
+        hasBlackJack = true;
+    } else {
+        message = "Bust"
+        isInGame = false;
+    }
+
+}
